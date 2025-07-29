9b98d1cc300858b2b5533f95279e9681bb595ebe
9M2PJU <9m2pju@gmail.com>
2025-07-30 00:46:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -106,7 +106,7 @@ var aIMG = [
   ],
   [
     "RAIN",
-    "iframe|https://www.rainviewer.com/weather-radar-map-live.html?loc=3.9415,107.7775,4&oC=true&oCS=1&c=3&o=83&lm=1&layer=radar&sm=1&sn=1"
+    "iframe|https://www.rainviewer.com/weather-radar-map-live.html?loc=3.9415,107.7775,4&oCS=1&c=3&o=83&lm=1&layer=radar&sm=1&sn=1"
   ],
   [
     "LIGHTNING",
