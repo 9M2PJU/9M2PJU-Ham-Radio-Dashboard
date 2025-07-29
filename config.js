c6c2f4b9d4e91debaf43b8450a5607cffafddf3a
9M2PJU <9m2pju@gmail.com>
2025-07-29 16:13:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -106,7 +106,7 @@ var aIMG = [
   ],
   [
     "RAIN",
-    "iframe|https://www.rainviewer.com/weather-radar-map-live.html?loc=4.2045,107.0304,5&oC=true&oCS=1&c=3&o=83&lm=1&layer=radar&sm=1&sn=1"
+    "iframe|https://assets.weather-forecast.com/maps/static/Malaysia.prec.3.cc23.jpg"
   ],
   [
     "LIGHTNING",
