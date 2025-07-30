713d31ed8191e27506a97e990f33968f18bbb691
9M2PJU <9m2pju@hamradio.my>
2025-07-30 21:24:31 +0800

    last

--- config.js
+++ config.js
@@ -110,7 +110,7 @@ var aIMG = [
   [
     "GREY LINE",
     "https://www.timeanddate.com/scripts/sunmap.php?iso=now",
-    "https://dx.qsl.net/images/greyline-20235.jpg" 
+    "iframe|https://dx.qsl.net/images/greyline-20235.jpg" 
   ],
   [
     "NOAA D-RAP",
